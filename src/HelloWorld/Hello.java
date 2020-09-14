f81aaac74de6a5b7fae455e7fa3cf9ab429ea2b0
sakchaisr <sakchaisr@gmail.com>
2020-09-14 15:23:26 +0700

    2nd commit

--- src/HelloWorld/Hello.java
+++ src/HelloWorld/Hello.java
@@ -3,5 +3,6 @@ package HelloWorld;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hi Java");
+        System.out.println("Mr.sakchai srimakorn");
     }
 }
